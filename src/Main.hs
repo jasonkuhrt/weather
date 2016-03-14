6d0e46f09ec46137b69098081b55292deda23665
Jason Kuhrt <jasonkuhrt@me.com>
2016-03-14 15:02:34 -0400

    Make a successful request to the forecast.io API. This simply proves that we can read from the API. In this case we are getting a summary for the upcoming day or so of weather in human-language (English) terms.
    Note that this commit exposes an API token linked to our forecast.io account.
    This should not be a big deal given this is not highly publicized and anyways
    no credit card is linked to our account. Later on this will be improved such
    that e.g. our program will read the API Access Token from some Home Directory
    file.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Network.Wreq
+import Control.Lens
+import Data.Aeson.Lens
+import Data.Maybe
+
+
+
+apiHost = "https://api.forecast.io/forecast/"
+apiKey = "db4db26b0bb9053853a23e8565ce7be9/"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  r <- get (apiHost ++ apiKey ++ "45.501689,-73.567256")
+  let summary = fromJust $ r ^? responseBody . key "hourly" . key "summary"
+  print summary

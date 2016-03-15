d578fd45e8030d08f6c6272217827a13129bf039
Jason Kuhrt <jasonkuhrt@me.com>
2016-03-14 20:57:13 -0400

    Refactor
    Begin a DSL for working with with the forecast.io API. This includes more
    types and functions for getting the weather given an AccessToken and Location.

--- src/Main.hs
+++ src/Main.hs
@@ -6,14 +6,48 @@ import Network.Wreq
 import Control.Lens
 import Data.Aeson.Lens
 import Data.Maybe
+import Data.ByteString.Lazy
 
 
+
+type AccessToken = String
+
+data Location =
+  Location {
+    latitude :: Float,
+    longitude :: Float
+  }
+  deriving (Show, Eq)
+
+
+
+-- Forcast.io API
 
 apiHost = "https://api.forecast.io/forecast/"
-apiKey = "db4db26b0bb9053853a23e8565ce7be9/"
+
+getWeather :: AccessToken -> Location -> IO (Response ByteString)
+getWeather accessToken (Location lat lon) = get uri
+  where
+    uri = apiHost ++ accessToken ++ "/" ++ (show lat) ++ "," ++ (show lon)
+
+-- Location Aliases
+
+montreal = Location 45.501689 (-73.567256)
+
+
+
+-- API Helpers
+
+accessToken = "db4db26b0bb9053853a23e8565ce7be9"
+getWeatherIn = getWeather accessToken
+
+
+
+
+
 
 main :: IO ()
 main = do
-  r <- get (apiHost ++ apiKey ++ "45.501689,-73.567256")
+  r <- getWeatherIn montreal
   let summary = fromJust $ r ^? responseBody . key "hourly" . key "summary"
   print summary
